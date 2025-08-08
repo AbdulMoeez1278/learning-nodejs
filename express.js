e45bb9e4900e992f413bc5b803bb326d7b9c0262
Abdul Moeez Tariq <150109036+AbdulMoeez1278@users.noreply.github.com>
2025-08-09 01:51:25 +0500

    Update learning-nodejs

--- express.js
+++ express.js
@@ -1,6 +1,10 @@
+// rendering HTML and  JSON
+
 const express = require("express");
 const app = express();
-const port = 3000;
+
+const hostname = "127.0.0.1"; // specifies the hostname
+const port = 3000; // specifies the port
 
 app.get("/", (req, res) => {
   // console.log("Data send by browser =>) ", req.query.name); // if we have this query or request from client : 127.0.0.1:3000?name=moeez
@@ -8,13 +12,28 @@ app.get("/", (req, res) => {
   // we can also send that query or request from the server
   // res.send("Welcome " + req.query.name);
 
-  //   console.log("Hello World");
-  res.send("<h1>Home page</h1>");
+  console.log("Home Page");
+  res.send(`<h1>Home page</h1>
+    <a href="/about">About Page</a>
+    `);
 });
 
 app.get("/about", (req, res) => {
   console.log("About Page");
-  res.send("<h1>About page</h1>");
+  // res.send("<h1>About page</h1>");
+
+  // rendering html
+  // res.send(`
+  //   <input type = "text" placeholder = "Enter your text" />
+  //   <button>Click Me</button>
+  // `);
+
+  // Interview question  (<----
+  // if we have to send or get the query parameters data inside the input tag
+  res.send(`
+    <input type = "text" placeholder = "Enter your text" value= "${req.query.name}" />
+    <button>Click Me</button>
+    `);
 });
 
 app.get("/blog", (req, res) => {
@@ -27,6 +46,28 @@ app.get("/contact", (req, res) => {
   res.send("<h1>Contact Page</h1>");
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on this port: 127.0.0.1:${port}`);
+// rendering json
+app.get("/help", (req, res) => {
+  res.send([
+    {
+      name: "Moeez",
+      email: "moeez@gmail.com",
+    },
+    {
+      name: "Sohaib",
+      email: "sohaib@gmail.com",
+    },
+  ]);
+});
+
+// using slug
+// app.get("/users/:userId/posts/:postId", (req, res) => {
+//   const userId = req.params.userId; // Access the userId parameter
+//   const postId = req.params.postId; // Access the postId parameter
+//   // ... further processing using userId and postId
+//   res.send(`User ID: ${userId}, Post ID: ${postId}`);
+// });
+
+app.listen(port, hostname, () => {
+  console.log(`Server is listening on this port: ${hostname}:${port}`);
 });
---
+++ public.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// directory path
+const publicPath = path.join(__dirname, "public");
+
+// EJS template engine
+app.set("view engine", "ejs");
+
+const port = 5000; // specifies the port
+const hostname = "127.0.0.1"; // specifies the hostname
+
+// if we have to remove extentions from the url's then we don't have to use express.static method
+// app.use(express.static(publicPath));
+
+// we use app.get method with res.send file inside it, to remove the  extentions from the url's we have to use sendFile
+app.get("/", (req, res) => {
+  res.sendFile(`${publicPath}/index.html`); // remove the extension from the URL + shows the static content
+  console.log("Home Page");
+});
+
+// serve EJS file using dynamic routing - sending data from the server
+app.get("/profile", (req, res) => {
+  // create an object of a user to make the profile page dynamic
+  const user = {
+    name: "Moeez",
+    email: "moeez@gmail.com",
+    city: "Lahore",
+  };
+  res.render("profile", { user }); // passed it as an object
+});
+
+// about page
+app.get("/about", (req, res) => {
+  res.sendFile(`${publicPath}/about.html`);// remove the extension from the URL + shows the static content
+  console.log("About Page");
+});
+
+// blog page
+app.get("/blog", (req, res) => {
+  res.sendFile(`${publicPath}/blog.html`);// remove the extension from the URL + shows the static content
+  console.log("Blog Page");
+});
+
+// contact page
+app.get("/contact", (req, res) => {
+  res.sendFile(`${publicPath}/contact.html`);// remove the extension from the URL + shows the static content
+  console.log("Contact Page");
+});
+
+// 404 page
+// Express v5+ — valid wildcard - first method to use the *splat
+app.get("/*splat", (req, res) => {
+  res.sendFile(`${publicPath}/404.html`);// remove the extension from the URL + shows the static content
+  console.log("Page Not Found!");
+});
+
+// Express v5+ — valid wildcard - second method to use the *catchcall
+// app.get("/*catchall", (req, res) => {
+//   res.sendFile(`${publicPath}/404.html`);
+//   console.log("Page Not Found!");
+// });
+
+// server listening the port
+app.listen(port, hostname, () => {
+  console.log(`Server is listening on ${hostname}:${port}`);
+});
