d48423dbb44ea8e8335a0f377acbba744ab2c7aa
Abdul Moeez Tariq <150109036+AbdulMoeez1278@users.noreply.github.com>
2025-08-20 23:14:25 +0500

    Update learning.js

---
+++ basicMongooseAPI/getAPI.js
@@ -0,0 +1,32 @@
+// GET API
+const express = require("express");
+require("./mongoConfig"); // import mongoConfig file
+
+// import our Schemas or Models file
+const product = require("./product");
+
+// initialize an express as app || import the app in express
+const app = express();
+
+// how to ensure that the data comes in nodejs or not - we use middleware
+app.use(express.json()); // parse the string data into json
+
+const port = 5000; // initilaized the port
+const hostname = "127.0.0.1"; // initialized the hostname
+
+// GET API Route - read data from postman to db
+app.get("/list", async (req, res) => {
+  // use find() method to find the product from the database
+  let data = await product.find(); // find() method returns the promise - to handle that promise we use async/await
+
+  // send the data as an response
+  res.send(data); // read the data from mongodb database
+
+  //   // prints the result on the terminal
+  //   console.log(data);
+});
+
+// listening the server
+app.listen(port, hostname, () => {
+  console.log(`Server is listening on ${hostname}:${port}`);
+});
---
+++ basicMongooseAPI/putAPI.js
@@ -0,0 +1,32 @@
+// PUT API
+const express = require("express");
+require("./mongoConfig"); // import mongoConfig file
+
+// import our Schemas or Models file
+const product = require("./product");
+
+// initialize an express as app || import the app in express
+const app = express();
+
+// how to ensure that the data comes in nodejs or not - we use middleware
+app.use(express.json()); // parse the string data into json
+
+const port = 5000; // initilaized the port
+const hostname = "127.0.0.1"; // initialized the hostname
+
+// PUT API Route - update data from postman to db
+app.put("/delete/:_id", async (req, res) => {
+  // use insertOne() method to update the single product from the database
+  let data = await product.insertOne(req.params); // insertOne() method also returns the promise - to handle that promise we use async/await
+
+  // send the data as an response
+  res.send(data); // read the data from mongodb database
+
+  //   // prints the result on the terminal
+  //   console.log(data);
+});
+
+// listening the server
+app.listen(port, hostname, () => {
+  console.log(`Server is listening on ${hostname}:${port}`);
+});
