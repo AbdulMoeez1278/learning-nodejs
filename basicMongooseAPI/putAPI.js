1659a1d40c5bc7acc9250188383f84f5f500457d
AbdulMoeez1278 <150109036+AbdulMoeez1278@users.noreply.github.com>
2025-08-21 02:29:23 +0500

    Update putAPI.js

--- basicMongooseAPI/putAPI.js
+++ basicMongooseAPI/putAPI.js
@@ -15,9 +15,11 @@ const port = 5000; // initilaized the port
 const hostname = "127.0.0.1"; // initialized the hostname
 
 // PUT API Route - update data from postman to db
-app.put("/delete/:_id", async (req, res) => {
-  // use insertOne() method to update the single product from the database
-  let data = await product.insertOne(req.params); // insertOne() method also returns the promise - to handle that promise we use async/await
+app.put("/update/:_id", async (req, res) => {
+  // use updateOne() method to update the single product from the database
+  let data = await product.updateOne(req.params, {
+    $set: req.body,
+  }); // updateOne() method also returns the promise - to handle that promise we use async/await
 
   // send the data as an response
   res.send(data); // read the data from mongodb database
